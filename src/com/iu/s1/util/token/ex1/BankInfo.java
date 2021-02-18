5876618db6fecbbf6bace9a70fe3c978a0db9b21
Daeki <kimdaeki@gmail.com>
2021-02-18 14:42:12 +0900

    1

--- src/com/iu/s1/util/token/ex1/BankInfo.java
+++ src/com/iu/s1/util/token/ex1/BankInfo.java
@@ -1,7 +1,9 @@
 package com.iu.s1.util.token.ex1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class BankInfo {
@@ -10,11 +12,35 @@ public class BankInfo {
 	
 	public BankInfo() {
 		sb = new StringBuffer();
-		sb.append("국민,마이핏,1.2,500,");
-		sb.append("우리,직장인우대,1.4,2000,");
-		sb.append("신한,자유적금,0.8,5000,");
-		sb.append("기업,나라사랑,1.9,4000");
+		sb.append("kb,국민,마이핏,1.2,500,");
+		sb.append("woo,우리,직장인우대,1.4,2000,");
+		sb.append("shin,신한,자유적금,0.8,5000,");
+		sb.append("ki,기업,나라사랑,1.9,4000");
 	}
+	
+	//init2 HashMap
+	//파싱 StringTokenizer
+	public Map<String, BankBookDTO> init2() {
+		String str = sb.toString();
+		
+		StringTokenizer st = new StringTokenizer(str, ",");
+		HashMap<String, BankBookDTO> map = new HashMap<>();
+		
+		while(st.hasMoreTokens()) {
+			BankBookDTO bankBookDTO = new BankBookDTO();
+			String key = st.nextToken();
+			bankBookDTO.setBank(st.nextToken());
+			bankBookDTO.setName(st.nextToken());
+			bankBookDTO.setRate(Double.parseDouble(st.nextToken()));
+			bankBookDTO.setMoney(Integer.parseInt(st.nextToken()));
+			map.put(key, bankBookDTO);
+		}
+		
+		return map;
+		
+	}
+	
+	
 	
 	//init ArrayList
 	//파싱 StringTokenizer
--- src/com/iu/s1/util/token/ex1/Ex1Main.java
+++ src/com/iu/s1/util/token/ex1/Ex1Main.java
@@ -1,21 +1,42 @@
 package com.iu.s1.util.token.ex1;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Ex1Main {
 
 	public static void main(String[] args) {
 		
 		BankInfo bankInfo = new BankInfo();
-		List<BankBookDTO> ar = bankInfo.init();
+		Map<String, BankBookDTO> map = bankInfo.init2();
 		
-		for(int i=0;i<ar.size();i++) {
-			System.out.println(ar.get(i).getBank());
-			System.out.println(ar.get(i).getName());
-			System.out.println(ar.get(i).getRate());
-			System.out.println(ar.get(i).getMoney());
-			System.out.println("---------------------");
+		Set<String> set = map.keySet();
+		
+		Iterator<String> iterator = set.iterator();
+		
+		while(iterator.hasNext()) {
+			String key = iterator.next();
+			BankBookDTO bankBookDTO = map.get(key);
+			
+			System.out.println(bankBookDTO.getBank());
+			System.out.println(bankBookDTO.getName());
+			System.out.println(bankBookDTO.getRate());
+			System.out.println(bankBookDTO.getMoney());
+			System.out.println("--------------------------");
 		}
+		
+		
+//		List<BankBookDTO> ar = bankInfo.init();
+//		
+//		for(int i=0;i<ar.size();i++) {
+//			System.out.println(ar.get(i).getBank());
+//			System.out.println(ar.get(i).getName());
+//			System.out.println(ar.get(i).getRate());
+//			System.out.println(ar.get(i).getMoney());
+//			System.out.println("---------------------");
+//		}
 		
 	}
 
